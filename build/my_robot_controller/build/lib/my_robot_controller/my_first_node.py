f06ca1d5d03931a3f66fd5ec71bb78a0a2779511
Giselly Reis <gisellyareis@gmail.com>
2024-07-26 09:11:54 -0300

    basic ros2 node

--- build/my_robot_controller/build/lib/my_robot_controller/my_first_node.py
+++ build/my_robot_controller/build/lib/my_robot_controller/my_first_node.py
@@ -8,12 +8,7 @@ class MyNode(Node):
 
     def __init__(self):
         super().__init__("first_node")
-        self.counter_ = 0
-        self.create_timer(1.0, self.timer_callback)
-    
-    def timer_callback(self):
-        self.get_logger().info("Hello " + str(self.counter_))
-        self.counter_ += 1
+        self.get_logger().info("Hello from ROS2 ")
 
 def main(args=None):
     rclpy.init(args=args)
---
+++ build/my_robot_controller/setup.py
@@ -0,0 +1,1 @@
+/home/giselly/ros2_ws/src/my_robot_controller/setup.py
